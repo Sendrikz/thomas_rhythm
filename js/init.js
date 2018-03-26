64a32b5888f67c096ea58c8187548d59920b0ff4
Sendrikz <yuryeva.olga99@gmail.com>
2018-03-27 02:14:28 +0300

    add slick(what people say)

--- js/init.js
+++ js/init.js
@@ -3,5 +3,7 @@ $( function() {
     // Init slick carousel
     $('.reviews-carousel').slick({
       dots:true,
+      prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+      nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>'
     });
   } );
