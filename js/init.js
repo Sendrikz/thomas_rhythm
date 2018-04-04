f4f9d8fadfbba82344a680d4a45d9007fc092249
Sendrikz <yuryeva.olga99@gmail.com>
2018-04-04 21:41:33 +0300

    adaptive for 991

--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 $( function() {
+    // Init tabs
     $( "#tabs" ).tabs();
     $("#portfolio-tabs").tabs();
     // Init slick carousel
@@ -7,4 +8,9 @@ $( function() {
       prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
       nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>'
     });
-  } );
+    // Init slick-nav (burger)
+    $('.header-nav').slicknav({
+      appendTo: '.header .container-fluid',
+      label: ''
+    });
+  });
